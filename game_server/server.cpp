7bf1eecd96950525bd0f2642e4dffb344d670d78
Christopher Walker <cnwalker@uchicago.edu>
2017-05-04 17:18:38 -0500

    styling

--- game_server/server.cpp
+++ game_server/server.cpp
@@ -11,75 +11,70 @@
 #include <ctime>
 #include <iostream>
 #include <string>
-#include <boost/array.hpp>
+#include <boost/asio.hpp>
 #include <boost/bind.hpp>
+#include <boost/array.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/asio.hpp>
 #include "JSON.h"
 
 using boost::asio::ip::udp;
 
-class GameLoopServer
-{
-public:
-  GameLoopServer(boost::asio::io_service& io_service)
-    : socket_(io_service, udp::endpoint(udp::v4(), 8000))
-  {
-    start_receive();
-  }
+class GameLoopServer {
+    public:
+        GameLoopServer(boost::asio::io_service& io_service)
+        : socket_(io_service, udp::endpoint(udp::v4(), 8000)) {
+            start_receive();
+        }
 
-private:
-  void start_receive()
-  {
-    socket_.async_receive_from(
-        boost::asio::buffer(recv_buffer_), remote_endpoint_,
-        boost::bind(&GameLoopServer::handle_receive, this,
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
-  }
+    private:
+        void start_receive() {
+            socket_.async_receive_from(
+                boost::asio::buffer(recv_buffer_), remote_endpoint_,
+                boost::bind(
+                    &GameLoopServer::handle_receive, this,
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred
+                ));
+        }
 
-  void handle_receive(const boost::system::error_code& error,
-      std::size_t /*bytes_transferred*/)
-  {
-    if (!error || error == boost::asio::error::message_size)
-    {
+        void handle_receive(const boost::system::error_code& error,
+            std::size_t /*bytes_transferred*/) {
+                if (!error || error == boost::asio::error::message_size) {
+                    // Success handler
+                    char *lol = (char*)malloc(sizeof(char)*10);
+                    std::string msg_to_send = "Hello World";
+                    boost::shared_ptr<std::string> message(new std::string(msg_to_send));
 
-        char *lol = (char*)malloc(sizeof(char)*10);
-        std::string msg_to_send = "Hello World";
+                    // Sets a listener that responds to requests asynchronusly
+                    socket_.async_send_to(
+                        boost::asio::buffer(*message), remote_endpoint_,
+                        boost::bind(&GameLoopServer::handle_send, this, message,
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::bytes_transferred));
 
-      boost::shared_ptr<std::string> message(new std::string(msg_to_send));
+                    start_receive();
+                }
+            }
 
-      socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
-          boost::bind(&GameLoopServer::handle_send, this, message,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
+        void handle_send(boost::shared_ptr<std::string> /*message*/,
+            const boost::system::error_code& /*error*/,
+            std::size_t /*bytes_transferred*/) {
 
-      start_receive();
-    }
-  }
+            }
 
-
-  void handle_send(boost::shared_ptr<std::string> /*message*/,
-      const boost::system::error_code& /*error*/,
-      std::size_t /*bytes_transferred*/)
-  {
-  }
-
-  udp::socket socket_;
-  udp::endpoint remote_endpoint_;
-  boost::array<char, 1> recv_buffer_;
-};
+        udp::socket socket_;
+        udp::endpoint remote_endpoint_;
+        boost::array<char, 1> recv_buffer_;
+    };
 
 int main()
 {
-  try
-  {
+  try {
     boost::asio::io_service io_service;
     GameLoopServer server(io_service);
     io_service.run();
-  }
-  catch (std::exception& e)
-  {
+  } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
 
