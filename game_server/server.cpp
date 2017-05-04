7545f9bcc2ed69829f4cbcdda37b3a45f59473a0
Christopher Walker <cnwalker@uchicago.edu>
2017-05-04 17:03:29 -0500

    server on master, deleting server branch

--- game_server/server.cpp
+++ game_server/server.cpp
@@ -1,120 +1,86 @@
-/* 
- *
- * server.cpp - codename-ted (2017)
- *
- * The main server loop for the game
- * include the game logic, the interfacing with  networking, and
- * the physics engine and AI
- *
- * Worked on by Chris, David, and Jigar
- *
- */
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2016 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 
-#include <cstdlib>
+#include <ctime>
 #include <iostream>
-#include <sstream>
-#include <map>
-//#include "definitions.h"
-#include "TdBike.h"
+#include <string>
+#include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
 #include <boost/asio.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
 
-using boost::asio::ip::udp; //for the game loop
-using boost::asio::ip::tcp; //for registration
-using boost::property_tree::ptree;
-using boost::property_tree::read_json;
-using boost::property_tree::write_json;
+using boost::asio::ip::udp;
 
-enum { max_length = 1024 };
-
-/*void register(boost::asio::io_service& io_service, unsigned short port)
+class GameLoopServer
 {
-    //tcp::socket sock(io_service, tcp::endpoint(tcp::v4(), port));
-}*/
+public:
+  GameLoopServer(boost::asio::io_service& io_service)
+    : socket_(io_service, udp::endpoint(udp::v4(), 8000))
+  {
+    start_receive();
+  }
 
-void server(boost::asio::io_service& io_service, unsigned short port) {
-    //register(io_service, port);  
+private:
+  void start_receive()
+  {
+    socket_.async_receive_from(
+        boost::asio::buffer(recv_buffer_), remote_endpoint_,
+        boost::bind(&GameLoopServer::handle_receive, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
 
-    udp::socket sock(io_service, udp::endpoint(udp::v4(), port));
+  void handle_receive(const boost::system::error_code& error,
+      std::size_t /*bytes_transferred*/)
+  {
+    if (!error || error == boost::asio::error::message_size)
+    {
 
-    bool quit = false;
-    int code = 0;
-    TdBike bike = TdBike(200.0, 200.0, 500.0);
+        char *lol = (char*)malloc(sizeof(char)*10);
+        std::string msg_to_send = "Hello World";
 
-    while (true) {
-        code = 0;
+      boost::shared_ptr<std::string> message(new std::string(msg_to_send));
 
-        char data[max_length];
-        udp::endpoint sender_endpoint;
-        size_t length = sock.receive_from(
-                boost::asio::buffer(data, max_length), sender_endpoint);
+      socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
+          boost::bind(&GameLoopServer::handle_send, this, message,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
 
-        char input = data[0];
-
-        switch (input) {
-            case 'W':
-                if (bike.vel < 2.0) {
-                    bike.vel += 0.01;
-                }
-                break;
-            case 'A':
-                if (bike.wheel < 1.6f) {
-                    bike.wheel += 0.02;
-                }
-                break;
-            case 'S':
-                if (bike.vel > 0.0) {
-                    bike.vel -= 0.0095;
-                }
-                break;
-            case 'D':
-                if (bike.wheel > -1.6f) {
-                    bike.wheel -= 0.02;
-                }
-                break;
-            default : 
-                code = 1;
-        }
-
-        if (bike.vel > 0.0005) {
-            bike.vel -= 0.0005;
-        } else {
-            bike.vel = 0;
-        }
-
-        std::cout << input << std::endl;
-
-        ptree response;
-        response.put("code", code);
-        response.put("vel", (int)bike.vel);
-        response.put("wheel", (int)bike.wheel);
-        std::ostringstream buf;
-        write_json (buf, response, false);
-        std::string json = buf.str();
-        length = json.length();
-        json += '\0';
-
-        std::cout << "JSON:" << json << "\n";
-
-        sock.send_to(boost::asio::buffer(json, length+1), sender_endpoint);
+      start_receive();
     }
+  }
+
+
+  void handle_send(boost::shared_ptr<std::string> /*message*/,
+      const boost::system::error_code& /*error*/,
+      std::size_t /*bytes_transferred*/)
+  {
+  }
+
+  udp::socket socket_;
+  udp::endpoint remote_endpoint_;
+  boost::array<char, 1> recv_buffer_;
+};
+
+int main()
+{
+  try
+  {
+    boost::asio::io_service io_service;
+    GameLoopServer server(io_service);
+    io_service.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
 }
-
-int main(int argc, char* argv[]) {
-    try {
-        if (argc != 2) {
-            std::cerr << "Usage: blocking_udp_echo_server <port>\n";
-            return 1;
-        }
-
-        boost::asio::io_service io_service;
-
-        server(io_service, std::atoi(argv[1]));
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
-
-    return 0;
-}
-
