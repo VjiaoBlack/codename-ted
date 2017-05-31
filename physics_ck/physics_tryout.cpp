3d1f5df2578f92565e961983ba99bce4bffc7bd6
Charlie K <charliek@wireless-s1-so-150-56-201.uchicago.edu>
2017-05-31 16:43:25 -0500

    Further tweaked physics collision results.  No more clipping, but orientations switching quickly is still off putting, will fix that soon

--- physics_ck/physics_tryout.cpp
+++ physics_ck/physics_tryout.cpp
@@ -314,11 +314,10 @@ PiGameMap compute_gamestate(unordered_map<string, vector<string> > input_object,
                 continue;
             }
             else{ //Checking collision with another ship
-                if(is_colliding(gm, current_boat, boat_to_compare, 0) && (array_empty_checker(merchantsColliding))){
-                    merchantsColliding[current_boat*5 + boat_to_compare] = 1;
+                if(is_colliding(gm, current_boat, boat_to_compare, 0)){
                     // If this passes, do more granular collision.  This is barebones so I'll just treat this as a collision.
                     //I am just going to reverse the accelerations for both ships to see if that fixes it.  I can do something more special later.
-                    if(1)
+                    if(verbose)
                         printf("Collided\n");
                     vec2 combined_dir = vec2(gm.merchants[current_boat].velocity.x + gm.merchants[boat_to_compare].velocity.x, 
                                             gm.merchants[current_boat].velocity.y + gm.merchants[boat_to_compare].velocity.y);
@@ -356,26 +355,31 @@ PiGameMap compute_gamestate(unordered_map<string, vector<string> > input_object,
                     gm.merchants[boat_to_compare].orientation = atan2(gm.merchants[boat_to_compare].velocity.y, gm.merchants[boat_to_compare].velocity.x) * 1/pioveroneeighty;
 
                     //Keep adding the velocity to the position until they don't collide
-                    // while((is_colliding(gm, current_boat, boat_to_compare)) || (is_colliding(gm, boat_to_compare, current_boat))){
-                    //     //Manually setting new positions
+                    while((is_colliding(gm, current_boat, boat_to_compare,0)) || (is_colliding(gm, boat_to_compare, current_boat,0))){
+                        //Manually setting new positions
 
-                    //     gm.merchants[current_boat].coord_pos.x += gm.merchants[current_boat].velocity.x * engineMultiplier;
-                    //     gm.merchants[current_boat].coord_pos.y += gm.merchants[current_boat].velocity.y * engineMultiplier;
+                        vec2 vector_for_curr_away_from_comp = vec2(gm.merchants[current_boat].coord_pos.x - gm.merchants[boat_to_compare].coord_pos.x,
+                                                                    gm.merchants[current_boat].coord_pos.y - gm.merchants[boat_to_compare].coord_pos.y);
+                        vec2 vector_for_comp_away_from_curr = vec2(gm.merchants[boat_to_compare].coord_pos.x - gm.merchants[current_boat].coord_pos.x,
+                                                                    gm.merchants[boat_to_compare].coord_pos.y - gm.merchants[current_boat].coord_pos.y);
 
-                    //     gm.merchants[boat_to_compare].coord_pos.x += gm.merchants[current_boat].velocity.x * engineMultiplier;
-                    //     gm.merchants[boat_to_compare].coord_pos.y += gm.merchants[current_boat].velocity.y * engineMultiplier;
+                        gm.merchants[current_boat].coord_pos.x += vector_for_curr_away_from_comp.Normalize().x * engineMultiplier;
+                        gm.merchants[current_boat].coord_pos.y += vector_for_curr_away_from_comp.Normalize().y * engineMultiplier;
 
-                    //     //Moving the merchant to the appropriate tile
-                    //     vec2 newLoc_curr = vec2(gm.merchants[current_boat].coord_pos.x,gm.merchants[current_boat].coord_pos.y);
-                    //     gm.merchants[current_boat].position = convert_coord_tile(gm, newLoc_curr);
+                        gm.merchants[boat_to_compare].coord_pos.x += vector_for_comp_away_from_curr.Normalize().x * engineMultiplier;
+                        gm.merchants[boat_to_compare].coord_pos.y += vector_for_comp_away_from_curr.Normalize().y * engineMultiplier;
 
-                    //     vec2 newLoc_comp = vec2(gm.merchants[boat_to_compare].coord_pos.x,gm.merchants[boat_to_compare].coord_pos.y);
-                    //     gm.merchants[boat_to_compare].position = convert_coord_tile(gm, newLoc_comp);
-                    // }
+                        //Moving the merchant to the appropriate tile
+                        vec2 newLoc_curr = vec2(gm.merchants[current_boat].coord_pos.x,gm.merchants[current_boat].coord_pos.y);
+                        gm.merchants[current_boat].position = convert_coord_tile(gm, newLoc_curr);
+
+                        vec2 newLoc_comp = vec2(gm.merchants[boat_to_compare].coord_pos.x,gm.merchants[boat_to_compare].coord_pos.y);
+                        gm.merchants[boat_to_compare].position = convert_coord_tile(gm, newLoc_comp);
+                    }
                 }
-                else if(!(is_colliding(gm, current_boat, boat_to_compare, 1)) && (merchantsColliding[current_boat*5 + boat_to_compare] == 1)){
-                    merchantsColliding[current_boat*5 + boat_to_compare] = 0;
-                }
+                // else if(!(is_colliding(gm, current_boat, boat_to_compare, 1)) && (merchantsColliding[current_boat*5 + boat_to_compare] == 1)){
+                //     merchantsColliding[current_boat*5 + boat_to_compare] = 0;
+                // }
                 else{ // Not close to each other so pass.
                     continue;
                 }
@@ -453,7 +457,7 @@ int main(int argc, char* argv[]){
     map.merchants[1].coord_pos.x = 50;
     map.merchants[1].coord_pos.y = 50;
 
-    map.merchants[1].velocity.x = .01;
+    //map.merchants[1].velocity.x = .01;
 
     map.merchants[1].goldAmount = 0;
     map.merchants[0].goldAmount = 1000;
